ba179be20ee22e6173cc9c552c122e05f7ae1cd3
xavieryuan <xavieryuan@gmail.com>
2013-09-04 15:16:22 +0800

    Redesigned UI

---
+++ js/DE.UIManager.js
@@ -0,0 +1,178 @@
+// JavaScript Document
+//全局方法整理
+
+/*
+DE.UIManager.showScreen(screenLink)		//显示特定的屏幕内容，screenLink是相应屏幕section标签的ID并带有#，例如#de_screen_project
+DE.UIManager.showSearchScreen(keyword,type)
+										//显示搜索结果屏幕，keyword是搜索关键词，type是优先显示的结果类型，type的值可以是resource或者project
+DE.UIManager.showFilterMenu()			//显示筛选菜单
+DE.UIManager.hideFilterMenu()			//隐藏筛选菜单
+DE.UIManager.showExtMenu()				//显示扩展菜单
+DE.UIManager.hideExtMenu()				//隐藏扩展菜单
+DE.UIManager.showProjectDetail()		//显示作品详情
+DE.UIManager.hideProjectDetail()		//隐藏作品详情
+DE.UIManager.hideAllMenuAndPopouts()	//隐藏所有菜单及弹出面板的状态，
+DE.UIManager.showLoginPopout()			//显示登录弹窗
+DE.UIManager.showRegPopout()			//显示注册弹窗
+DE.UIManager.showMsgPopout(title,msg)	//显示消息弹窗，title为消息框的标题，msg为消息正文
+DE.UIManager.showRestPwdPopout()		//显示重置密码弹窗
+DE.UIManager.hidePopout()				//关闭弹窗
+DE.UIManager.guestView()				//显示访客视图
+DE.UIManager.regView()					//显示注册用户视图
+DE.UIManager.vipView()					//显示VIP用户视图
+DE.UIManager.adminView()				//显示管理员视图
+
+*/
+
+var DE=DE||{};
+
+DE.UIManager=function(){
+	//私有属性及方法	
+	var showPopout=function(){
+		$("#de_popout").removeClass("de_hidden")
+		$("#de_blackout").removeClass("de_hidden")
+		}
+	var hidePopout=function(){
+		$("#de_popout").addClass("de_hidden")
+		$("#de_blackout").addClass("de_hidden")		
+		}
+	var gotoScreen=function(screenLink,paraObj){
+			cleanAllScreens()
+			$(screenLink).removeClass("de_hidden")
+			
+			//加载某些特定屏幕时需要额外地处理视图
+			if(screenLink=="#de_screen_project"){
+				$(".de_top_nav .de_btn_project").addClass("active")
+			}else if(screenLink=="#de_screen_resource"){
+				$(".de_top_nav .de_btn_resource").addClass("active")
+			}else if(screenLink=="#de_screen_designer"){
+				$(".de_top_nav .de_btn_designer").addClass("active")
+			}else if(screenLink=="#de_screen_search_result"){
+				if(paraObj && paraObj.keyword){
+					$("#de_btn_filter>a").html( "搜索："+paraObj.keyword ).addClass("active")
+					}
+				if(paraObj && paraObj.type=="resource"){
+					$("#de_screen_search_result>.de_category_filter>.de_category_filter_option_resource").addClass("active")
+					}else{
+					$("#de_screen_search_result>.de_category_filter>.de_category_filter_option_project").addClass("active")	
+					}
+			}else if(screenLink=="#de_screen_project_detail"){
+				
+				}
+		}
+	var cleanAllScreens=function(){
+		
+		DE.UIManager.hideAllMenuAndPopouts()
+		DE.UIManager.hideProjectDetail()
+				
+		$("#de_screen_container>section").addClass("de_hidden");
+		$("#de_btn_filter .active").html("更多分类...").removeClass("active");			
+		$("#de_top_nav .active").removeClass("active");
+		$("#de_screen_search_result>.de_category_filter .active").removeClass("active");
+		//如需释放内存可激活下列代码
+		//$("#de_screen_container>section>ul").empty();
+		}
+	
+	return{ 
+	//公共属性及方法 开始
+	showScreen:function(screenLink,paraObj){		
+			gotoScreen(screenLink)
+		},
+	showSearchScreen:function(keyword,type){
+			gotoScreen("#de_screen_search_result",{keyword:keyword,type:type})
+		},	
+	showFilterMenu:function(){
+		DE.UIManager.hideAllMenuAndPopouts()
+		$("#de_filter_menu").css("height",320)
+		$("#de_btn_filter>a").addClass("focused")
+		},
+	hideFilterMenu:function(){
+		$("#de_filter_menu").css("height",0);
+		$("#de_btn_filter>a").removeClass("focused")		
+		},
+	showExtMenu:function(){
+		DE.UIManager.hideAllMenuAndPopouts()
+		$("#de_ext_nav").css("right",0)
+		},
+	hideExtMenu:function(){
+		$("#de_ext_nav").css("right","-150px")
+		},
+	showProjectDetail:function(){
+		$("#de_screen_project_detail").removeClass("de_hidden");
+		$("#de_screen_project_detail").scrollTop(0)
+		$("body").addClass("de_noscroll");
+		},
+	hideProjectDetail:function(){
+		$("#de_screen_project_detail").addClass("de_hidden");
+		$("body").removeClass("de_noscroll");
+		},
+	showLoginPopout:function(){
+		showPopout()
+		$("#de_popout>.de_inner_wrapper").css("left",0);
+		$("#de_popout>.de_popout_title").html("登录");
+		$("#de_login_email").focus();
+		},
+	showRegPopout:function(){
+		showPopout()
+		$("#de_popout>.de_inner_wrapper").css("left","-100%");
+		$("#de_popout>.de_popout_title").html("注册");
+		$("#de_reg_username").focus();
+		},
+	showRecoverPwdPopout:function(){
+		showPopout()
+		$("#de_popout>.de_inner_wrapper").css("left","-200%");
+		$("#de_popout>.de_popout_title").html("找回密码");
+		$("#de_recover_pwd_email").focus();		
+		},
+	showMsgPopout:function(title,msg){
+		showPopout()
+		$("#de_popout .de_popout_title").html(title)
+		$("#de_popout .de_popout_msg").html(msg)
+		$("#de_popout>.de_inner_wrapper").css("left","-300%")
+		},
+	showRestPwdPopout:function(){
+		showPopout()
+		$("#de_popout>.de_inner_wrapper").css("left","-400%");
+		$("#de_popout>.de_popout_title").html("修改密码");
+		$("#de_reset_pwd").focus();		
+		},
+	showEditProfilePopout:function(){
+		showPopout()
+		$("#de_popout>.de_inner_wrapper").css("left","-500%");
+		$("#de_popout>.de_popout_title").html("编辑个人信息");
+		},
+	hidePopout:function(){
+		hidePopout()
+		},	
+	hideAllMenuAndPopouts:function(){
+		DE.UIManager.hideFilterMenu()
+		DE.UIManager.hideExtMenu()
+		hidePopout()
+		},
+	guestView:function(){
+		$(".de_guest_view").removeClass("de_hidden");
+		$(".de_reg_view").addClass("de_hidden");
+		$(".de_vip_view").addClass("de_hidden");
+		$(".de_admin_view").addClass("de_hidden");
+		},
+	regView:function(){
+		$(".de_guest_view").addClass("de_hidden");
+		$(".de_reg_view").removeClass("de_hidden");
+		$(".de_vip_view").addClass("de_hidden");
+		$(".de_admin_view").addClass("de_hidden");
+		},
+	vipView:function(){
+		$(".de_guest_view").addClass("de_hidden");
+		$(".de_reg_view").addClass("de_hidden");
+		$(".de_vip_view").removeClass("de_hidden");
+		$(".de_admin_view").addClass("de_hidden");
+		},
+	adminView:function(){
+		$(".de_guest_view").addClass("de_hidden");
+		$(".de_reg_view").addClass("de_hidden");
+		$(".de_vip_view").addClass("de_hidden");
+		$(".de_admin_view").removeClass("de_hidden");
+		}
+	//公共属性及方法 结束
+		}
+	}()
--- js/index.js
+++ js/index.js
@@ -1,49 +1,120 @@
 // JavaScript Document
 
-function hideAllMenu(){
-	$("#de_filter_menu").css("height",0)
-	}
-
-$(document).ready(function(e) {
-	//初始化
-	//绑定菜单事件
-	 $("#de_btn_project_filter").on("click",function(evt){
-		 if($("#de_filter_menu").height()<459){
-			 $("#de_filter_menu").css("height",459)
-			 }else{
-			 $("#de_filter_menu").css("height",0)	 
-			}
-			evt.stopPropagation();		 
-		 })
+$(document).ready(function(evt) {
+//初始化
 	
+//数据无关的初始化工作
+	//登录注册按钮点击事件
+	$(".de_btn_login_reg").on("click",function(evt){
+		DE.UIManager.showLoginPopout();
+		evt.stopPropagation();
+		})
+	//更多分类按钮点击事件	
+	$("#de_btn_filter>a").on("click",function(evt){
+		DE.UIManager.showFilterMenu()
+		evt.stopPropagation();		 
+		})
+	//ext菜单按钮点击事件
+	$("#de_btn_ext_nav").on("click",function(evt){
+		DE.UIManager.showExtMenu();	
+		evt.stopPropagation();	
+		})
+	//上传按钮点击事件
+	$("#de_btn_upload").on("click",function(evt){
+		DE.UIManager.showScreen($(this).attr("href"));		
+		evt.stopPropagation();	
+		})	
+	//注册按钮点击事件
+	$("#de_reg_btn").on("click",function(){
+		DE.UIManager.showRegPopout();
+		})
+	//忘记密码按钮点击事件	
+	$("#de_btn_forgot_pwd").on("click",function(){
+		DE.UIManager.showRecoverPwdPopout();
+		
+		})
+	//重置密码按钮点击事件
+	$("#de_btn_reset_pwd").on("click",function(){
+		DE.UIManager.showRestPwdPopout();
+		})
+	//修改个人信息按钮
+	$("#de_btn_edit_profile").on("click",function(){
+		DE.UIManager.showEditProfilePopout();
+		})
+	
+	
+	//关闭弹窗按钮点击事件
+	$("#de_popout .de_popout_close_btn").on("click",function(){
+		DE.UIManager.hidePopout();
+		})
+		
+	//点击body隐藏所有弹窗和菜单
+	$(document).on("click",function(){		
+		DE.UIManager.hideAllMenuAndPopouts()
+		})	
+	
+	//点击各类弹窗和菜单时，阻止点击事件冒泡到document
+	$("#de_filter_menu").on("click",function(evt){
+		evt.stopPropagation();
+		})
+	$("#de_ext_nav").on("click",function(evt){
+		evt.stopPropagation();
+		})
+	$("#de_popout").on("click",function(evt){		
+		evt.stopPropagation();
+		}) 		
+
+//数据相关的初始化
+	
+	//Filter Menu 按钮点击事件	
 	$("#de_filter_menu li>a").on("click",function(evt){
 		
-		$(this).addClass("selected")
-		$("#de_filter_menu li>a").not(this).removeClass("selected");
+		//DE.UIManager.showSearchResultScreen($(this).text())
+		console.log($(this).attr("href"))
+		DE.UIManager.showSearchScreen($(this).text(),"project")		
 		
-		$("#de_btn_project_filter").html( $(this).text() )
-		
-		})
-		 				 
-	$(document).on("click",function(){
-		hideAllMenu()
+		//开始查询代码		
 		})
 	
-	//section tab标签
-	$("#de_section_tab>ul>li>a").on("click",function(evt){
+	//top nav tab标签点击事件
+	$("#de_top_nav>ul>li>a").on("click",function(evt){				
+		DE.UIManager.showScreen($(this).attr("href"));
+		//开始查询代码
+		})
+	
+	
+
+	//临时登录
+	$("#de_btn_sign_in").on("click",function(evt){
+		DE.DataManager.doSignIn()
+		DE.UIManager.hideAllMenuAndPopouts()
 		
-		$("#de_section_tab>ul>li>a").each(function(index, ele) {
-            $(ele).removeClass("de_tab_active");
-			$($(ele).attr("href")).addClass("de_hidden")
-        });
+		})
+	//临时注销
+	$("#de_btn_sign_out").on("click",function(evt){
+		DE.UIManager.guestView()
+		DE.UIManager.hideAllMenuAndPopouts()
 		
-		var target=$(this).attr("href");
-		$(this).addClass("de_tab_active")
-		$(target).removeClass("de_hidden")
+		})
+	
+	//访问个人主页面
+	$("#de_btn_user_profile").on("click",function(evt){
+		DE.UIManager.showScreen($(this).attr("href"));		
+		})
 		
-		evt.preventDefault();
-		})	 
-			 
+	$(".user_info a").on("click",function(evt){
+		DE.UIManager.showScreen("#");		
+		})
+	
+	//显示作品详情
+	$(".project_info a").on("click",function(evt){
+		DE.UIManager.showProjectDetail()
+		})
+
+	//关闭作品详情
+	$("#de_btn_close_project_detail").on("click",function(evt){
+		DE.UIManager.hideProjectDetail()
+		})		 
 });
 
 
