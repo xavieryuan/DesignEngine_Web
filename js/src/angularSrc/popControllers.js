86bb3f0c6bde471d8a517943b4822decf31d5ec5
csboyty <csboyty@gmail.com>
2014-08-05 18:03:11 +0800

    更新上传模块

---
+++ js/src/angularSrc/popControllers.js
@@ -0,0 +1,129 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ty
+ * Date: 14-8-5
+ * Time: 上午9:52
+ * To change this template use File | Settings | File Templates.
+ */
+var popControllers=angular.module("popControllers",["classes"]);
+popControllers.controller("signIn",["$scope","Config","LocationChanger","Storage","CFunctions",
+    function($scope,Config,LocationChanger,Storage,CFunctions){
+
+        $scope.loginError="";
+        $scope.popFlags.title=Config.titles.signIn;
+        $scope.popFlags.showPop=true;
+        $scope.mainFlags.showBlackOut=true;
+
+        $scope.toRegPanel=function(){
+            $scope.popFlags.popTemplateUrl=Config.templateUrls.signUp;
+            LocationChanger.skipReload().withoutRefresh(Config.urls.register,false);
+        };
+        $scope.forgetPwd=function(){
+            $scope.popFlags.popTemplateUrl=Config.templateUrls.forgetPwd;
+            LocationChanger.skipReload().withoutRefresh(Config.urls.forgetPwd,false);
+        };
+
+        if(document.cookie){
+            var obj=JSON.parse(decodeURIComponent(document.cookie));
+            $scope.login.email=obj.email;
+            $scope.login.password=obj.password;
+            $scope.login.rememberMe=true;
+        }
+
+        //记住我
+        function rememberMe(){
+            if($scope.login.rememberMe){
+                var email= $scope.login.email;
+                var password=$scope.login.password;
+                var obj={
+                    "email":email,
+                    "password":password
+                };
+                document.cookie = encodeURIComponent(JSON.stringify(obj))+"; max-age=7*24*60*60; path=/";
+            }else{
+                document.cookie="";
+            }
+        }
+        $scope.loginSubmit=function(){
+            //rememberMe();
+            Storage.initCurrentUser({
+                userId:2,
+                profile:"data/people1.jpg",
+                role:"admin",
+                name:"测试用户",
+                email:"csboyty@163.com",
+                description:"测试用户的说明"
+            });
+
+            $scope.closePop();
+        };
+
+
+    }]);
+
+popControllers.controller("signUp",["$scope","CFunctions","Config",function($scope,CFunctions,Config){
+    $scope.popFlags.title=Config.titles.signUp;
+    $scope.popFlags.showPop=true;
+    $scope.mainFlags.showBlackOut=true;
+    $scope.registerError="";
+    $scope.captcha="captcha.jpg";
+
+    $scope.refreshCaptcha=function(){
+        $scope.captha=$scope.captha+"?"+Math.random();
+    };
+
+    $scope.registerSubmit=function(){
+
+    };
+}]);
+
+popControllers.controller("forgetPwd",["$scope","CFunctions","Config",function($scope,CFunctions,Config){
+    $scope.popFlags.title=Config.titles.forgetPwd;
+    $scope.popFlags.showPop=true;
+    $scope.mainFlags.showBlackOut=true;
+
+    $scope.forgetPwdSubmit=function(){
+
+    };
+
+}]);
+
+popControllers.controller("search",["$scope","Config","LocationChanger",function($scope,Config,LocationChanger){
+    $scope.popFlags.title=Config.titles.search;
+    $scope.popFlags.showPop=true;
+    $scope.mainFlags.showBlackOut=true;
+    $scope.mainFlags.extMenuClass="";
+
+    $scope.toSearch=function(href){
+        LocationChanger.canReload().withoutRefresh(href,false);
+        $scope.popFlags.showPop=false;
+        $scope.mainFlags.showBlackOut=false;
+        $scope.popFlags.popTemplateUrl="";
+    }
+
+}]);
+
+popControllers.controller("editPwd",["$scope","CFunctions","Config",function($scope,CFunctions,Config){
+    $scope.popFlags.title=Config.titles.editPwd;
+    $scope.popFlags.showPop=true;
+    $scope.mainFlags.showBlackOut=true;
+
+    $scope.editPwdSubmit=function(){
+
+    };
+
+}]);
+
+popControllers.controller("editInfo",["$scope","CFunctions","Config",function($scope,CFunctions,Config){
+    $scope.popFlags.title=Config.titles.editInfo;
+    $scope.popFlags.showPop=true;
+    $scope.mainFlags.showBlackOut=true;
+    $scope.editInfo.profile=$scope.currentUser.profile;
+    $scope.editInfo.description=$scope.currentUser.description;
+
+    $scope.editInfoSubmit=function(){
+
+    };
+
+}]);
+
--- js/src/angularSrc/viewControllers.js
+++ js/src/angularSrc/viewControllers.js
@@ -12,128 +12,9 @@
  * Time: 下午3:42
  * To change this template use File | Settings | File Templates.
  */
-var controllers=angular.module("controllers",["classes"]);
-controllers.controller("signIn",["$scope","Config","LocationChanger","Storage","CFunctions",
-    function($scope,Config,LocationChanger,Storage,CFunctions){
+var viewControllers=angular.module("viewControllers",["classes"]);
 
-        $scope.loginError="";
-        $scope.popFlags.title=Config.titles.signIn;
-        $scope.popFlags.showPop=true;
-        $scope.mainFlags.showBlackOut=true;
-
-        $scope.toRegPanel=function(){
-            $scope.popFlags.popTemplateUrl=Config.templateUrls.signUp;
-            LocationChanger.skipReload().withoutRefresh(Config.urls.register,false);
-        };
-        $scope.forgetPwd=function(){
-            $scope.popFlags.popTemplateUrl=Config.templateUrls.forgetPwd;
-            LocationChanger.skipReload().withoutRefresh(Config.urls.forgetPwd,false);
-        };
-
-        if(document.cookie){
-            var obj=JSON.parse(decodeURIComponent(document.cookie));
-            $scope.login.email=obj.email;
-            $scope.login.password=obj.password;
-            $scope.login.rememberMe=true;
-        }
-
-        //记住我
-        function rememberMe(){
-            if($scope.login.rememberMe){
-                var email= $scope.login.email;
-                var password=$scope.login.password;
-                var obj={
-                    "email":email,
-                    "password":password
-                };
-                document.cookie = encodeURIComponent(JSON.stringify(obj))+"; max-age=7*24*60*60; path=/";
-            }else{
-                document.cookie="";
-            }
-        }
-        $scope.loginSubmit=function(){
-            //rememberMe();
-            Storage.initCurrentUser({
-                userId:2,
-                profile:"data/people1.jpg",
-                role:"admin",
-                name:"测试用户",
-                email:"csboyty@163.com",
-                description:"测试用户的说明"
-            });
-
-            $scope.closePop();
-        };
-
-
-    }]);
-
-controllers.controller("signUp",["$scope","CFunctions","Config",function($scope,CFunctions,Config){
-    $scope.popFlags.title=Config.titles.signUp;
-    $scope.popFlags.showPop=true;
-    $scope.mainFlags.showBlackOut=true;
-    $scope.registerError="";
-    $scope.captcha="captcha.jpg";
-
-    $scope.refreshCaptcha=function(){
-        $scope.captha=$scope.captha+"?"+Math.random();
-    };
-
-    $scope.registerSubmit=function(){
-
-    };
-}]);
-
-controllers.controller("forgetPwd",["$scope","CFunctions","Config",function($scope,CFunctions,Config){
-    $scope.popFlags.title=Config.titles.forgetPwd;
-    $scope.popFlags.showPop=true;
-    $scope.mainFlags.showBlackOut=true;
-
-    $scope.forgetPwdSubmit=function(){
-
-    };
-
-}]);
-
-controllers.controller("search",["$scope","Config","LocationChanger",function($scope,Config,LocationChanger){
-    $scope.popFlags.title=Config.titles.search;
-    $scope.popFlags.showPop=true;
-    $scope.mainFlags.showBlackOut=true;
-    $scope.mainFlags.extMenuClass="";
-
-    $scope.toSearch=function(href){
-        LocationChanger.canReload().withoutRefresh(href,false);
-        $scope.popFlags.showPop=false;
-        $scope.mainFlags.showBlackOut=false;
-        $scope.popFlags.popTemplateUrl="";
-    }
-
-}]);
-
-controllers.controller("editPwd",["$scope","CFunctions","Config",function($scope,CFunctions,Config){
-    $scope.popFlags.title=Config.titles.editPwd;
-    $scope.popFlags.showPop=true;
-    $scope.mainFlags.showBlackOut=true;
-
-    $scope.editPwdSubmit=function(){
-
-    };
-
-}]);
-controllers.controller("editInfo",["$scope","CFunctions","Config",function($scope,CFunctions,Config){
-    $scope.popFlags.title=Config.titles.editInfo;
-    $scope.popFlags.showPop=true;
-    $scope.mainFlags.showBlackOut=true;
-    $scope.editInfo.profile=$scope.currentUser.profile;
-    $scope.editInfo.description=$scope.currentUser.description;
-
-    $scope.editInfoSubmit=function(){
-
-    };
-
-}]);
-
-controllers.controller("showProjects",['$scope',"Config","Project","CFunctions",function($scope,Config,Project,CFunctions){
+viewControllers.controller("showProjects",['$scope',"Config","Project","CFunctions",function($scope,Config,Project,CFunctions){
 
     //覆盖了super里面的，一定要分开写，不然无法覆盖（这样可以覆盖的原理是因为对象是地址类型）
     $scope.menuStatus.projectsClass=Config.classNames.mainMenuActive;
@@ -149,7 +30,7 @@ controllers.controller("showProjects",['$scope',"Config","Project","CFunctions",
 
 }]);
 
-controllers.controller("showBoxes",['$scope',"Config",function($scope,Config){
+viewControllers.controller("showBoxes",['$scope',"Config",function($scope,Config){
 
     //覆盖了super里面的，一定要分开写，不然无法覆盖（这样可以覆盖的原理是因为对象是地址类型）
     $scope.menuStatus.projectsClass="";
@@ -299,7 +180,7 @@ controllers.controller("showBoxes",['$scope',"Config",function($scope,Config){
 
 }]);
 
-controllers.controller("userHome",['$scope',function($scope){
+viewControllers.controller("userHome",['$scope',function($scope){
 
     //覆盖了super里面的，一定要分开写，不然无法覆盖（这样可以覆盖的原理是因为对象是地址类型）
     $scope.menuStatus.projectsClass="";
@@ -388,7 +269,7 @@ controllers.controller("userHome",['$scope',function($scope){
 
 }]);
 
-controllers.controller("projectDetail",["$scope","LocationChanger",function($scope,LocationChanger){
+viewControllers.controller("projectDetail",["$scope","LocationChanger",function($scope,LocationChanger){
 
     $scope.project={
         "praised":true,
@@ -521,7 +402,7 @@ controllers.controller("projectDetail",["$scope","LocationChanger",function($sco
     ];
 }]);
 
-controllers.controller("searchResult",["$scope","LocationChanger",function($scope,LocationChanger){
+viewControllers.controller("searchResult",["$scope","LocationChanger",function($scope,LocationChanger){
     $scope.menuStatus.projectsClass="";
     $scope.menuStatus.boxesClass="";
 
@@ -551,7 +432,7 @@ controllers.controller("searchResult",["$scope","LocationChanger",function($scop
     ];
 }]);
 
-controllers.controller("uploadProject",["$scope","$route","Config","CFunctions",function($scope,$route,Config,CFunctions){
+viewControllers.controller("uploadProject",["$scope","$route","Config","CFunctions",function($scope,$route,Config,CFunctions){
 
     function addTag(tag){
         if($scope.uploadProject.tags.indexOf(tag)===-1){
@@ -591,7 +472,7 @@ controllers.controller("uploadProject",["$scope","$route","Config","CFunctions",
                 $scope.uploadProject.medias[mediaId][Config.mediaObj.mediaThumbFilePath]=path;
                 $scope.uploadProject.medias[mediaId][Config.mediaObj.mediaThumbFilename]=file.name;
                 $scope.uploadProject.medias[mediaId][Config.mediaObj.mediaFilename]=file.name;
-                $scope.uploadProject.medias[mediaId][Config.mediaObj.mediaFilepath]=path;
+                $scope.uploadProject.medias[mediaId][Config.mediaObj.mediaFilePath]=path;
                 $scope.uploadProject.medias[mediaId][Config.mediaObj.mediaMemo]="";
                 $scope.uploadProject.medias[mediaId][Config.mediaObj.mediaTitle]="";
 
@@ -600,7 +481,8 @@ controllers.controller("uploadProject",["$scope","$route","Config","CFunctions",
                 /*$scope.uploadProject.medias[mediaId][Config.mediaObj.mediaThumbFilePath]=path;
                 $scope.uploadProject.medias[mediaId][Config.mediaObj.mediaThumbFilename]=file.name;*/
                 $scope.uploadProject.medias[mediaId][Config.mediaObj.mediaFilename]=file.name;
-                $scope.uploadProject.medias[mediaId][Config.mediaObj.mediaFilepath]=path;
+                $scope.uploadProject.medias[mediaId]["noThumb"]=true;
+                $scope.uploadProject.medias[mediaId][Config.mediaObj.mediaFilePath]=path;
                 $scope.uploadProject.medias[mediaId][Config.mediaObj.mediaMemo]="";
                 $scope.uploadProject.medias[mediaId][Config.mediaObj.mediaTitle]="";
 
@@ -609,17 +491,58 @@ controllers.controller("uploadProject",["$scope","$route","Config","CFunctions",
 
         $scope.$apply();
     }
+    function createMediaUploader(buttonId,containerId,type){
+        var filter="";
+        var maxSize=0;
+        switch(type){
+            case Config.mediaTypes.image:
+                filter=Config.mediaFilters.image;
+                maxSize=Config.uploadSize.maxImageSize;
+                break;
+            case Config.mediaTypes.mp4:
+                filter=Config.mediaFilters.mp4;
+                maxSize=Config.uploadSize.maxMediaSize;
+                break;
+            case Config.mediaTypes.zip:
+                filter=Config.mediaFilters.zip;
+                maxSize=Config.uploadSize.maxMediaSize;
+                break;
+            case Config.mediaTypes.swf:
+                filter=Config.mediaFilters.swf;
+                maxSize=Config.uploadSize.maxMediaSize;
+                break;
+        }
+
+        CFunctions.createUploader({
+            browseButton:buttonId,
+            multiSelection:true,
+            container:containerId,
+            multipartParams:null,
+            maxSize:maxSize,
+            filter:filter,
+            fileAddCb:function(up,files){
+                fileAddedCb(files,type);
+            },
+            progressCb:progressCb,
+            uploadedCb:function(file,info){
+                fileUploadedCb(file,info,type);
+            }
+        });
+    }
 
     CFunctions.drag();
     var fileIdToMediaIdHash={};
 
     $scope.uploadProject={
-        currentEditMediaId:"",
         thumb:Config.thumbs.defaultThumb,
         tags:[],
         medias:{}
     };
 
+    $scope.currentEditMediaId=0;
+    $scope.mediaSetPanelUrl="";
+    $scope.medias=[];
+    $scope.mediaSetTitle=Config.messages.clickToSet;
     $scope.mediaMenuActive=Config.classNames.mediaMenuActive;
     $scope.stepClass=[Config.classNames.uploadStepActive,"",""];
     $scope.stepPanelClass=["",Config.classNames.hidden,Config.classNames.hidden];
@@ -632,6 +555,25 @@ controllers.controller("uploadProject",["$scope","$route","Config","CFunctions",
             }else{
                 $scope.stepClass[i]="";
                 $scope.stepPanelClass[i]=Config.classNames.hidden;
+            }
+        }
+
+        //在点击的时候需要去掉媒体文件的active状态
+        if($scope.currentEditMediaId&&index!=2){
+            $scope.uploadProject.medias[$scope.currentEditMediaId]["active"]=undefined;
+            delete $scope.uploadProject.medias[$scope.currentEditMediaId]["active"];
+            $scope.currentEditMediaId=0;
+            $scope.mediaSetTitle=Config.messages.clickToSet;
+            $scope.mediaSetPanelUrl="";
+        }
+
+
+        if(index==2){
+            var mediaItems=document.getElementsByClassName("mediaItem");
+            var length=mediaItems.length;
+            for(var i=0;i<length;i++){
+                //console.log(mediaItems[i].getAttribute('data-media-id'));
+                $scope.medias.push($scope.uploadProject.medias[mediaItems[i].getAttribute('data-media-id')]);
             }
         }
     };
@@ -650,16 +592,16 @@ controllers.controller("uploadProject",["$scope","$route","Config","CFunctions",
         addTag(tag);
     };
 
-    $scope.createThumbUploader=function(id,containerId){
+    $scope.createThumbUploader=function(buttonId,containerId){
         CFunctions.createUploader({
-            browseButton:id,
+            browseButton:buttonId,
             multiSelection:false,
             container:containerId,
             multipartParams:null,
             maxSize:Config.uploadSize.maxImageSize,
             fileAddCb:null,
             progressCb:null,
-            filter:Config.mediaFilters.imageFilter,
+            filter:Config.mediaFilters.image,
             uploadedCb:function(file,info){
                 var res = JSON.parse(info);
                 $scope.uploadProject.thumb = Config.qNBucketDomain + res.key; //获取上传成功后的文件的Url
@@ -675,99 +617,121 @@ controllers.controller("uploadProject",["$scope","$route","Config","CFunctions",
     $scope.hideMediaMenu=function(){
         $scope.mediaMenuActive="";
     };
+
+    $scope.createImageUploader=function(buttonId,containerId){
+        createMediaUploader(buttonId,containerId,Config.mediaTypes.image);
+    };
+
+    $scope.createFileUploader=function(buttonId,containerId){
+        createMediaUploader(buttonId,containerId,Config.mediaTypes.zip);
+    };
+
+    $scope.createVideoUploader=function(buttonId,containerId){
+        createMediaUploader(buttonId,containerId,Config.mediaTypes.mp4);
+    };
+    $scope.createFlashUploader=function(buttonId,containerId){
+        createMediaUploader(buttonId,containerId,Config.mediaTypes.swf);
+    };
     $scope.deleteMedia=function(mediaId){
         if(confirm("确定删除此媒体文件吗？")){
             $scope.uploadProject.medias[mediaId]=undefined;
             delete $scope.uploadProject.medias[mediaId];
+            $scope.currentEditMediaId=0;
+            $scope.mediaSetPanelUrl="";
+            $scope.mediaSetTitle=Config.messages.clickToSet;
         }
     };
 
-    $scope.createImageUploader=function(id,containerId){
+    /*****************媒体设置部分********************/
+    $scope.showSetPanel=function(mediaId,mediaType){
+
+        //移除原来的元素的活动状态
+        if($scope.currentEditMediaId){
+            $scope.uploadProject.medias[$scope.currentEditMediaId]["active"]=undefined;
+            delete  $scope.uploadProject.medias[$scope.currentEditMediaId]["active"];
+        }
+
+        $scope.currentEditMediaId=mediaId;
+        $scope.mediaSetPanelUrl=Config.mediaSetPanelUrls[mediaType];
+        $scope.mediaSetTitle=Config.mediaTitles[mediaType];
+        $scope.currentThumbSrc=$scope.uploadProject.medias[mediaId][Config.mediaObj.mediaThumbFilePath];
+        $scope.currentFilename=$scope.uploadProject.medias[mediaId][Config.mediaObj.mediaFilename];
+        $scope.uploadProject.medias[mediaId]["active"]=true;
+    };
+
+    $scope.setMediaTitle=function(title){
+        $scope.uploadProject.medias[$scope.currentEditMediaId][Config.mediaObj.mediaTitle]=title;
+    };
+    $scope.setMediaMemo=function(memo){
+        $scope.uploadProject.medias[$scope.currentEditMediaId][Config.mediaObj.mediaMemo]=memo;
+    };
+    $scope.mediaSetThumbUploader=function(buttonId,containerId){
         CFunctions.createUploader({
-            browseButton:id,
-            multiSelection:true,
+            browseButton:buttonId,
+            multiSelection:false,
             container:containerId,
             multipartParams:null,
             maxSize:Config.uploadSize.maxImageSize,
-            filter:Config.mediaFilters.imageFilter,
-            fileAddCb:function(up,files){
-                fileAddedCb(files,Config.mediaTypes.image);
-            },
-            progressCb:progressCb,
+            fileAddCb:null,
+            progressCb:null,
+            filter:Config.mediaFilters.image,
             uploadedCb:function(file,info){
-                fileUploadedCb(file,info,Config.mediaTypes.image);
+                var res = JSON.parse(info);
+                var path=Config.qNBucketDomain + res.key; //获取上传成功后的文件的Url
+                $scope.uploadProject.medias[$scope.currentEditMediaId][Config.mediaObj.mediaThumbFilePath] =path;
+                $scope.uploadProject.medias[$scope.currentEditMediaId]["noThumb"]=undefined;
+                delete $scope.uploadProject.medias[$scope.currentEditMediaId]["noThumb"];
+                $scope.currentThumbSrc=path;
+
+                $scope.$apply();
             }
         });
     };
 
-    $scope.createFileUploader=function(id,containerId){
-        CFunctions.createUploader({
-            browseButton:id,
-            multiSelection:false,
-            container:containerId,
-            multipartParams:null,
-            maxSize:Config.uploadSize.maxMediaSize,
-            filter:Config.mediaFilters.zipFiler,
-            fileAddCb:function(up,files){
-                fileAddedCb(files,Config.mediaTypes.zip);
-            },
-            progressCb:progressCb,
-            uploadedCb:function(file,info){
-                fileUploadedCb(file,info,Config.mediaTypes.zip);
-            }
 
-        });
-    };
-    $scope.createPptUploader=function(id,containerId){
+    function mediaSetUploader(buttonId,containerId,type){
+        var filter="";
+        switch(type){
+            case Config.mediaTypes.mp4:
+                filter=Config.mediaFilters.mp4;
+                break;
+            case Config.mediaTypes.zip:
+                filter=Config.mediaFilters.zip;
+                break;
+            case Config.mediaTypes.swf:
+                filter=Config.mediaFilters.swf;
+                break;
+        }
+
         CFunctions.createUploader({
-            browseButton:id,
+            browseButton:buttonId,
             multiSelection:false,
             container:containerId,
             multipartParams:null,
+            filter:filter,
             maxSize:Config.uploadSize.maxMediaSize,
-            filter:Config.mediaFilters.pptFilter,
-            fileAddCb:function(up,files){
-                fileAddedCb(files,Config.mediaTypes.ppt);
+            fileAddCb:function(up,file){
+                $scope.uploadProject.medias[$scope.currentEditMediaId][Config.mediaObj.mediaThumbFilename]=
+                    file.name+"----0%";
+                $scope.$apply();
             },
-            progressCb:progressCb,
+            progressCb:function(up,file){
+                $scope.uploadProject.medias[$scope.currentEditMediaId][Config.mediaObj.mediaThumbFilename]=
+                    file.name+file.percent+"%";
+                $scope.$apply();
+            },
             uploadedCb:function(file,info){
-                fileUploadedCb(file,info,Config.mediaTypes.ppt);
+                var res = JSON.parse(info);
+                var path=Config.qNBucketDomain + res.key; //获取上传成功后的文件的Url
+                $scope.uploadProject.medias[$scope.currentEditMediaId][Config.mediaObj.mediaThumbFilename] =file.name;
+                $scope.uploadProject.medias[$scope.currentEditMediaId][Config.mediaObj.mediaFilePath] =path;
+
+                $scope.$apply();
             }
         });
-    };
-    $scope.createVideoUploader=function(id,containerId){
-        CFunctions.createUploader({
-            browseButton:id,
-            multiSelection:true,
-            container:containerId,
-            multipartParams:null,
-            maxSize:Config.uploadSize.maxMediaSize,
-            filter:Config.mediaFilters.mp4Filter,
-            fileAddCb:function(up,files){
-                fileAddedCb(files,Config.mediaTypes.mp4);
-            },
-            progressCb:progressCb,
-            uploadedCb:function(file,info){
-                fileUploadedCb(file,info,Config.mediaTypes.mp4);
-            }
-        });
-    };
-    $scope.createFlashUploader=function(id,containerId){
-        CFunctions.createUploader({
-            browseButton:id,
-            multiSelection:true,
-            container:containerId,
-            multipartParams:null,
-            maxSize:Config.uploadSize.maxMediaSize,
-            filter:Config.mediaFilters.swfFilter,
-            fileAddCb:function(up,files){
-                fileAddedCb(files,Config.mediaTypes.swf);
-            },
-            progressCb:progressCb,
-            uploadedCb:function(file,info){
-                fileUploadedCb(file,info,Config.mediaTypes.swf);
-            }
-        });
+    }
+    $scope.fileSetUploader=function(buttonId,containerId){
+        mediaSetUploader(buttonId,containerId,Config.mediaTypes.zip);
     }
 
 }]);
