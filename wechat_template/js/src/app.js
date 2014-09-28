a5e479bcc087d581ec0adab244fb145aa339275b
csboyty <csboyty@gmail.com>
2014-09-28 15:18:36 +0800

    调试

--- wechat_template/js/src/app.js
+++ wechat_template/js/src/app.js
@@ -49,59 +49,61 @@ weChatApp.directive("overwriteWeiXin",function(){
     return {
         link: function (scope, element, attrs, ctrl) {
             scope.$watch("project",function(newValue){
-                console.log(newValue);
-                var imgUrl = scope.project.profile_image;
-                var lineLink = location.href;
-                var descContent = scope.project.description;
-                var shareTitle = scope.project.name;
-                var appid = '';
-                function shareFriend() {
-                    WeixinJSBridge.invoke('sendAppMessage',{
-                        "appid": appid,
-                        "img_url": imgUrl,
-                        "img_width": "200",
-                        "img_height": "200",
-                        "link": lineLink,
-                        "desc": descContent,
-                        "title": shareTitle
-                    }, function(res) {
-                    })
-                }
-                function shareTimeline() {
-                    WeixinJSBridge.invoke('shareTimeline',{
-                        "img_url": imgUrl,
-                        "img_width": "200",
-                        "img_height": "200",
-                        "link": lineLink,
-                        "desc": descContent,
-                        "title": shareTitle
-                    }, function(res) {
-                    });
-                }
-                function shareWeibo() {
-                    WeixinJSBridge.invoke('shareWeibo',{
-                        "content": descContent,
-                        "url": lineLink
-                    }, function(res) {
+                if(!angular.equals({},newValue)){
+                    var imgUrl = scope.project.profile_image;
+                    var lineLink = location.href;
+                    var descContent = scope.project.description;
+                    var shareTitle = scope.project.name;
+                    var appid = '';
+                    function shareFriend() {
+                        WeixinJSBridge.invoke('sendAppMessage',{
+                            "appid": appid,
+                            "img_url": imgUrl,
+                            "img_width": "200",
+                            "img_height": "200",
+                            "link": lineLink,
+                            "desc": descContent,
+                            "title": shareTitle
+                        }, function(res) {
+                        })
+                    }
+                    function shareTimeline() {
+                        WeixinJSBridge.invoke('shareTimeline',{
+                            "img_url": imgUrl,
+                            "img_width": "200",
+                            "img_height": "200",
+                            "link": lineLink,
+                            "desc": descContent,
+                            "title": shareTitle
+                        }, function(res) {
+                        });
+                    }
+                    function shareWeibo() {
+                        WeixinJSBridge.invoke('shareWeibo',{
+                            "content": descContent,
+                            "url": lineLink
+                        }, function(res) {
 
-                    });
+                        });
+                    }
+
+                    // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+                    document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+                        // 发送给好友
+                        WeixinJSBridge.on('menu:share:appmessage', function(argv){
+                            shareFriend();
+                        });
+                        // 分享到朋友圈
+                        WeixinJSBridge.on('menu:share:timeline', function(argv){
+                            shareTimeline();
+                        });
+                        // 分享到微博
+                        WeixinJSBridge.on('menu:share:weibo', function(argv){
+                            shareWeibo();
+                        });
+                    }, false);
                 }
 
-                // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
-                document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-                    // 发送给好友
-                    WeixinJSBridge.on('menu:share:appmessage', function(argv){
-                        shareFriend();
-                    });
-                    // 分享到朋友圈
-                    WeixinJSBridge.on('menu:share:timeline', function(argv){
-                        shareTimeline();
-                    });
-                    // 分享到微博
-                    WeixinJSBridge.on('menu:share:weibo', function(argv){
-                        shareWeibo();
-                    });
-                }, false);
             });
 
         }
