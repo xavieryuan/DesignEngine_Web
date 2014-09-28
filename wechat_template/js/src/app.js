3eefa3e26b910659624fa04264d912aa031c2df5
csboyty <csboyty@gmail.com>
2014-09-28 14:41:39 +0800

    调试

--- wechat_template/js/src/app.js
+++ wechat_template/js/src/app.js
@@ -26,6 +26,26 @@ weChatApp.service("CFunctions",["$rootScope","$location",function($rootScope,$lo
     };
 }]);
 
+weChatApp.controller("projectDetail",["$scope","$http","Config","CFunctions",function($scope,$http,Config,CFunctions){
+    console.log("controller init");
+    var projectId=CFunctions.getPathParam();
+    $scope.project={};
+    $http.get(Config.ajaxUrls.getProjectDetail.replace(":projectId",projectId)).success(function(data){
+        $scope.project=data.artifact;
+        $scope.project.boxId=data.artifact.topic_id||data.user.id;
+        $scope.project.praised=data.praised;
+        $scope.project.user=data.user;
+        $scope.project.topic=data.topic;
+
+        var length=$scope.project.assets.length;
+        for(var i=0;i<length;i++){
+            var fileInfo=CFunctions.getFilePathInfo($scope.project.assets[i]["profile_image"]);
+            $scope.project.assets[i]["profile_image"]=
+                fileInfo["filePath"]+Config.imageScale.previewSmall+fileInfo["ext"];
+        }
+    });
+}]);
+
 weChatApp.directive("overwriteWeiXin",function(){
     return {
         link: function (scope, element, attrs, ctrl) {
@@ -88,22 +108,3 @@ weChatApp.directive("overwriteWeiXin",function(){
     }
 });
 
-weChatApp.controller("projectDetail",["$scope","$http","Config","CFunctions",function($scope,$http,Config,CFunctions){
-    console.log("controller init");
-    var projectId=CFunctions.getPathParam();
-    $scope.project={};
-    $http.get(Config.ajaxUrls.getProjectDetail.replace(":projectId",projectId)).success(function(data){
-        $scope.project=data.artifact;
-        $scope.project.boxId=data.artifact.topic_id||data.user.id;
-        $scope.project.praised=data.praised;
-        $scope.project.user=data.user;
-        $scope.project.topic=data.topic;
-
-        var length=$scope.project.assets.length;
-        for(var i=0;i<length;i++){
-            var fileInfo=CFunctions.getFilePathInfo($scope.project.assets[i]["profile_image"]);
-            $scope.project.assets[i]["profile_image"]=
-                fileInfo["filePath"]+Config.imageScale.previewSmall+fileInfo["ext"];
-        }
-    });
-}]);
