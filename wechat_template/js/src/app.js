5dcb7fab3ed2a06d1111bfebf0cfeffce942990f
csboyty <csboyty@gmail.com>
2014-09-29 11:20:32 +0800

    调试

--- wechat_template/js/src/app.js
+++ wechat_template/js/src/app.js
@@ -37,9 +37,6 @@ weChatApp.controller("projectDetail",["$scope","$http","Config","CFunctions",fun
         $scope.project.user=data.user;
         $scope.project.topic=data.topic;
 
-        shareData.imgUrl=$scope.project.profile_image;
-        shareData.shareTitle=$scope.project.name;
-        shareData.descContent=$scope.project.description;
 
         var length=$scope.project.assets.length;
         for(var i=0;i<length;i++){
