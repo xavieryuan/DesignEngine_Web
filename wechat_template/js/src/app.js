04ebf20de12a2d5b8032831fd8adfbd946398a29
csboyty <csboyty@gmail.com>
2014-09-29 11:15:13 +0800

    调试

--- wechat_template/js/src/app.js
+++ wechat_template/js/src/app.js
@@ -97,7 +97,7 @@ weChatApp.directive("overWeiXin",function(){
 
                     // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
                     document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-                        //alert("ready");
+                        alert("ready test");
                         // 发送给好友
                         WeixinJSBridge.on('menu:share:appmessage', function(argv){
                             shareFriend();
