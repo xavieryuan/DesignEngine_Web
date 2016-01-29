6114dbea4a6603da8628e7cd0ba5fac6b2b35744
xavieryuan <xavieryuan@xavierdeMacBook-Pro.local>
2016-01-29 17:49:05 +0800

    微信调试

--- wechat_template/js/src/app.js
+++ wechat_template/js/src/app.js
@@ -36,6 +36,7 @@ weChatApp.controller("projectDetail",["$scope","$http","Config","CFunctions",fun
         $scope.project.praised=data.praised;
         $scope.project.user=data.user;
         $scope.project.topic=data.topic;
+        console.log(data.topic)
         $scope.project.visible=data.artifact.visible;
 
         shareData.imgUrl=$scope.project.profile_image;
